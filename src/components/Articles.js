32faf0f9565181767df8c56c3a126ec7b19cf49d
Afpa <Afpa@POR7302929.exchange.ad.afpanet>
2022-03-02 14:19:24 +0100

    new commit

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -6,44 +6,72 @@ import Card from './Card';
 const Articles = () => {
 
     const [data, setData] = useState([]);
+    const [sortedData, setSortedData] = useState([]);
+
+    const [playOnce, setPlayOnce] = useState(true);
+    const [rangeValue, setRangeValue] = useState(5);
+
+    const [radioValue, setRadioValue] = useState('');
+    const radio = ['men\'s clothing', 'jewelery', 'electronics', 'women\'s clothing'];
+
+    let range = 20;
 
     useEffect(() => {
-        axios
-            .get('https://fakestoreapi.com/products')
-            .then((res) => setData(res.data));
+        if (playOnce) {
+            axios
+                .get('https://fakestoreapi.com/products')
+                .then((res) => {
+                    setData(res.data);
+                    setPlayOnce(false);
+                });
+        }
 
-    }, []);
+        const sortedData = () => {
+            const articleObj = Object.keys(data).map((i) => data[i]);
+
+            articleObj.length = rangeValue; //nombre élément à afficher
+            setSortedData(articleObj);
+        };
+
+        sortedData();
+
+    }, [data, rangeValue, playOnce]);
 
     return (
         <div className="container">
             <div className="row">
                 <div className="col-12 my-3 bg-dark text-white py-3">
-                    <label htmlFor="customRange1" className="form-label">Example range</label>
-                    <input type="range" className="form-range" id="customRange1" />
+                    <label htmlFor="customRange1" className="form-label">Nombre d'article affiché</label>
+                    <input type="range" className="form-range" value={rangeValue} onChange={(e) => setRangeValue(e.target.value)} min="1" max={range} />
                     <div className=' d-flex justify-content-around'>
-                        <div>
-                            <input className="form-check-input" type="checkbox" value="" id="first-marque" />
-                            <label className="form-check-label mr-1" htmlFor="first-marque">Default checkbox</label>
-                        </div>
-                        <div>
-                            <input className="form-check-input" type="checkbox" value="" id="sec-marque" />
-                            <label className="form-check-label" htmlFor="sec-marque">Default checkbox</label>
-                        </div>
-                        <div>
-                            <input className="form-check-input" type="checkbox" value="" id="t-marque" />
-                            <label className="form-check-label" htmlFor="t-marque">Default checkbox</label>
-                        </div>
-                        <div>
-                            <input className="form-check-input" type="checkbox" value="" id="f-marque" />
-                            <label className="form-check-label" htmlFor="f-marque">Default checkbox</label>
-                        </div>
+                        {radio.map((target) => {
+                            return (
+                                <div key={target}>
+                                    <input type="radio" value={target} id={target} checked={target === radioValue} onChange={(e) => {
+                                        return (
+                                            setRadioValue(e.target.value),
+                                            setRangeValue(data.length)
+                                        )
+                                    }} />
+                                    <label className="form-check-label mr-1" htmlFor={target}>{target}</label>
+                                </div>
+                            );
+                        })}
+
                     </div>
                 </div>
             </div>
+            <div className="mb-5">
+                {radioValue && <button onClick={() => setRadioValue("")} type="button" className="btn btn-danger">Annuler la recherche</button>}
+            </div>
             <div className="col-12">
-                {data.map((target) => (
-                    <Card article={target} key={target.id} />
-                ))}
+                {sortedData
+                    .filter((target) => {
+                        return target.category.includes(radioValue);
+                    })
+                    .map((target) => (
+                        <Card article={target} key={target.id} />
+                    ))}
             </div>
         </div>
     );
